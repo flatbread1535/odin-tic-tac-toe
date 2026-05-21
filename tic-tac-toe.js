ca5e9bd4afc0167d2ecdc030715529412bdcc9f2
Adam <ajlarson0731@gmail.com>
2026-05-21 19:32:26 -0400

    Created initial factory functions for the game board, player, and game flow.

---
+++ tic-tac-toe.js
@@ -0,0 +1,19 @@
+// Gameboard factory function
+function gameBoard() {
+
+    const board = [
+        [],
+        [],
+        [],
+    ]
+}
+
+// Player factory function
+function player(name, marker) {
+    return { name, marker };
+}
+
+// Gameflow factory function
+function gameFlow() {
+
+}
